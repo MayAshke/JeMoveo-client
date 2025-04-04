592a09983017edf07ac1613b3c8c9249d575d9a7
MayAshk <mayas@mta.ac.il>
2025-04-04 18:12:09 +0300

    hard coded server url

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const SERVER_URL = process.env.SERVER_URL;
+const SERVER_URL = "https://jamoveo-server-0597.onrender.com";
 
 const Signup = () => {
   const [username, setUsername] = useState('');
